5b046bcd8ea7f9b96332e42d2df26000fe9b2a88
lruler <1204312199@qq.com>
2022-01-20 23:59:04 +0800

    完成koa2框架搭建

--- be/node/bin/www.js
+++
@@ -1,9 +0,0 @@
-const http = require('http')
-const serverHandle = require('../app')
-
-const PORT = 8000
-
-const server = http.createServer(serverHandle)
-console.log('开启访问成功,请访问8000端口')
-
-server.listen(PORT)
---
+++ be/routes/users.js
@@ -0,0 +1,72 @@
+const models = require('../db/models')
+const router = require('koa-router')()
+
+router.prefix('/users')
+
+router.get('/', function (ctx, next) {
+  ctx.body = 'this is a users response!'
+})
+
+router.get('/bar', function (ctx, next) {
+  ctx.body = 'this is a users/bar response'
+})
+
+router.post('/add', async (ctx) => {
+  try {
+    const { Username, password} = ctx.request.body
+
+    // create 增加
+    let user = await models.User.create({
+      userName,
+      password
+    })
+
+    ctx.body = {
+      user,
+      msg: '创建成功！'
+    }
+
+  } catch (error) {
+    throw error
+  }
+
+})
+
+router.post('/update', async (ctx) => {
+  try {
+    const {userName, password, id } = ctx.request.body
+    // findOne 查找
+    let user = await models.User.findOne({
+      where: {
+        id
+      }
+    })
+    if (user) {
+      user = await user.update({
+        userName,
+        password,
+      })
+    }
+    ctx.body = {
+      user
+    }
+  } catch (error) {
+    throw error
+  }
+})
+
+router.get('/look', async () => {
+  const { page } = ctx.request.body
+  let limit = 10
+  let offect = (page - 1) * limit
+  models.User.findAndCountAll({
+    where: {
+      page
+    },
+    offect,
+    limit
+  })
+})
+
+
+module.exports = router
