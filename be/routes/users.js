1dc7c3d692323198480d3c1bfe968bfbee9d7ff8
lruler <1204312199@qq.com>
2022-01-22 23:00:27 +0800

    增加返回model

--- be/routes/users.js
+++ be/routes/users.js
@@ -1,4 +1,5 @@
 const models = require('../db/models')
+const { SuccessModel, ErrorModel } = require('../middleware/model')
 const router = require('koa-router')()
 
 router.prefix('/login')
@@ -16,40 +17,14 @@ router.post('/', async (ctx) => {
     })
 
     if (user) {
-      ctx.body = user.password === password ? {
-        code: 0,
-        msg: '登陆成功'
-      } : {
-        code: -1,
-        msg: '密码错误'
-      }
+      ctx.body = user.password === password ? new SuccessModel('登陆成功'): new ErrorModel('密码错误')
     } else {
-      ctx.body = {
-        code: -1,
-        msg: '该用户不存在'
-      }
+      ctx.body = new ErrorModel('用户不存在')
     }
 
   } catch (error) {
     throw error
   }
 })
-
-/*
-router.get('/look', async () => {
-  const { page } = ctx.request.body
-  let limit = 10
-  let offect = (page - 1) * limit
-  models.User.findAndCountAll({
-    where: {
-      page
-    },
-    offect,
-    limit
-  })
-})
-pm2 
-
-*/
 
 module.exports = router
