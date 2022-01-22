525143cd2a4be7f44d43829d97a0f96266b07f1e
lruler <1204312199@qq.com>
2022-01-22 11:31:30 +0800

    完善登陆逻辑

--- be/routes/users.js
+++ be/routes/users.js
@@ -7,27 +7,25 @@ const { User } = models
 
 router.post('/', async (ctx) => {
   try {
-    const {
-      userName,
-      password
-    } = ctx.request.body
+    const { userName, password } = ctx.request.body
 
-    console.log(userName, password);
-
-    let users = await User.findOne({
+    let user = await User.findOne({
       where: {
         userName,
-        password
       }
     })
 
-    if (users) {
-      ctx.body = {
-        users,
+    if (user) {
+      ctx.body = user.password === password ? {
+        code: 0,
         msg: '登陆成功'
+      } : {
+        code: -1,
+        msg: '密码错误'
       }
     } else {
       ctx.body = {
+        code: -1,
         msg: '该用户不存在'
       }
     }
